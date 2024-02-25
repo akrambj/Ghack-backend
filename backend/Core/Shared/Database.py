87b38a7a9c0016d62e95d95ba17901c8f5d88fff
Soapiane <Ls_yekene@esi.dz>
2024-02-25 16:00:39 +0100

    Added database operations

--- backend/Core/Shared/Database.py
+++ backend/Core/Shared/Database.py
@@ -6,7 +6,6 @@ import os
 
 CREDENTIALS_PATH = os.path.join(os.path.dirname(__file__), "../firebase.json")
 
-
 cred = credentials.Certificate(CREDENTIALS_PATH)
 firebase_admin.initialize_app(cred)
 
@@ -14,37 +13,24 @@ db = firestore.client()
 
 class Database:
     @staticmethod
-    def store(collection , document , query_dict):
+    def store(collection , document , dict):
         doc_ref = db.collection(collection).document(document)
-        try:
-            doc_ref.set(query_dict)
-            return (0,0)
-        except Exception as e:
-            return (1,str(e))
+        return doc_ref.set(dict)
 
 
     @staticmethod
     def read(collection , document):
         doc_ref = db.collection(collection).document(document)
-        try:
-            return (0,doc_ref.get())
-        except Exception as e:
-            return (1,str(e))
+        return doc_ref.get()
+ 
 
     @staticmethod
     def edit(collection , document,querydict):
         doc_ref = db.collection(collection).document(document)
-        try:
-            doc_ref.update(querydict)
-            return (0, 0)
-        except Exception as e:
-            return (1, str(e))
+        return  doc_ref.update(querydict)
+
 
     @staticmethod
     def delete(collection , document):
         event_ref = db.collection(collection).document(document)
-        try:
-            event_ref.delete()
-            return (0, 0)
-        except Exception as e:
-            return (1, str(e))
+        return event_ref.delete()
