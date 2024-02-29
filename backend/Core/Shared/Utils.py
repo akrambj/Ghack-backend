977a02f1176a5c44346d00217eccee24844815a4
soapiane <ls_yekene@esi.dz>
2024-02-29 22:25:25 +0100

    Fixed routes security issues

--- backend/Core/Shared/Utils.py
+++ backend/Core/Shared/Utils.py
@@ -1,6 +1,9 @@
 from Core.Shared.Database import Database , db
 from datetime import datetime
-
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi import status
+from Core.Shared.ErrorResponses import *
+from Core.env import HASHING_SECRET_KEY, HASH_ALGORITHM
 
 def formatUser(id):
     user = db.collection("users").document(id).get().to_dict()
@@ -25,3 +28,26 @@ def isDateCorrect(date):
     except ValueError:
         return False
     
+
+privilege_error = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail={
+        "success": False,
+        "message": "User not authorized to access this resource"},
+)
+
+bad_request_error = HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    detail={
+        "success": False,
+        "message": "Bad request"},
+)
+
+
+def projectProtected(userID, projectID):
+    project = Database.read("projects", projectID)
+    if project is None:
+        raise bad_request_error
+    if userID not in project["members"]:
+        raise privilege_error
+    return project
